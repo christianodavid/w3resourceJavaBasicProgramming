eabcb0780d54ea8a59bca6f041ea68793913fc70
Christiano <christianodvids@gmail.com>
2024-12-12 14:53:41 -0300

    feat: add productoftwonumbers file

---
+++ productoftwonumbers/src/ProductOfTwoNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ProductOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = multiply(num1, num2);
+
+        System.out.println(num1 + " * " + num2 + " = " + result);
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+}
